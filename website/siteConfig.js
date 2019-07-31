50204c38bcbb334a5b13462e4daed15ab6e8b712
Redned <RednedEpic@users.noreply.github.com>
2019-07-31 16:45:33 -0500

    Update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -54,8 +54,8 @@ const siteConfig = {
 
   /* Colors for website */
   colors: {
-    primaryColor: '#286917',
-    secondaryColor: '#1c4910',
+    primaryColor: '#13afd6',
+    secondaryColor: '#694919',
   },
 
   /* Custom fonts for website */
@@ -73,7 +73,7 @@ const siteConfig = {
   */
 
   // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
-  copyright: `Copyright © ${new Date().getFullYear()} Your Name or Your Company Name`,
+  copyright: `Copyright © ${new Date().getFullYear()} GeyserMC`,
 
   highlight: {
     // Highlight.js theme to use for syntax highlighting in code blocks.

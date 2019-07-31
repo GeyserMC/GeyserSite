7677c9fd73cf5f07d15c959093b387fa176adc55
RednedEpic <redned@retronixgames.x10host.com>
2019-07-31 17:28:33 -0500

    Update Footer.js

--- website/core/Footer.js
+++ website/core/Footer.js
@@ -48,73 +48,40 @@ class Footer extends React.Component {
             </a>
           </div>
           <div>
-            <h5>Community</h5>
-            <a href={this.pageUrl('users.html', this.props.language)}>
-              User Showcase
-            </a>
+            <h5>Links</h5>
             <a
-              href="https://stackoverflow.com/questions/tagged/"
+              href="https://bstats.org/plugin/server-implementation/Geyser"
               target="_blank"
               rel="noreferrer noopener">
-              Stack Overflow
+              Statistics
             </a>
-            <a href="https://discordapp.com/">Project Chat</a>
             <a
-              href="https://twitter.com/"
+              href="https://github.com/GeyserMC"
               target="_blank"
               rel="noreferrer noopener">
-              Twitter
+              GitHub
             </a>
+            <a href="https://discord.gg/mRjbCsS">Discord</a>
+            <a href={`${this.props.config.baseUrl}blog`}>Blog</a>
           </div>
           <div>
-            <h5>More</h5>
-            <a href={`${this.props.config.baseUrl}blog`}>Blog</a>
-            <a href="https://github.com/">GitHub</a>
-            <a
-              className="github-button"
-              href={this.props.config.repoUrl}
-              data-icon="octicon-star"
-              data-count-href="/facebook/docusaurus/stargazers"
-              data-show-count="true"
-              data-count-aria-label="# stargazers on GitHub"
-              aria-label="Star this project on GitHub">
-              Star
-            </a>
-            {this.props.config.twitterUsername && (
-              <div className="social">
-                <a
-                  href={`https://twitter.com/${
-                    this.props.config.twitterUsername
-                  }`}
-                  className="twitter-follow-button">
-                  Follow @{this.props.config.twitterUsername}
-                </a>
-              </div>
-            )}
-            {this.props.config.facebookAppId && (
-              <div className="social">
-                <div
-                  className="fb-like"
-                  data-href={this.props.config.url}
-                  data-colorscheme="dark"
-                  data-layout="standard"
-                  data-share="true"
-                  data-width="225"
-                  data-show-faces="false"
-                />
-              </div>
-            )}
+            <h5>Project Info</h5>
+            <img alt ="Stars" src="https://img.shields.io/github/stars/GeyserMC/Geyser?color=yellow&style=plastic">
+            <img alt ="Issues" src="https://img.shields.io/github/issues/GeyserMC/Geyser?color=red&style=plastic">
+            <img alt ="Pull Requests" src="https://img.shields.io/github/issues-pr/GeyserMC/Geyser?color=purple&style=plastic">
+            <img alt ="Forks" src="https://img.shields.io/github/forks/GeyserMC/Geyser">
+            <img alt ="Last Commit" src="https://img.shields.io/github/last-commit/GeyserMC/Geyser?color=green&style=plastic">
+            <img alt="License" src="https://img.shields.io/github/license/GeyserMC/Geyser?color=magenta&style=plastic">
           </div>
         </section>
-
         <a
-          href="https://opensource.facebook.com/"
+          href="https://github.com/GeyserMC/"
           target="_blank"
           rel="noreferrer noopener"
           className="fbOpenSource">
           <img
             src={`${this.props.config.baseUrl}img/oss_logo.png`}
-            alt="Facebook Open Source"
+            alt="GeyserMC"
             width="170"
             height="45"
           />

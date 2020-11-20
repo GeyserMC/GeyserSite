c752f9ae2b0fca66b85af7c8d1ab43391387225a
Olivia <chew@chew.pw>
2020-11-20 14:28:30 -0600

    Fix whatever Webstorm considers an error (#17)
    * Fix whatever Webstorm considers an error
    
    * Evidently this needs to be an h4

--- js/config-editor.js
+++ js/config-editor.js
@@ -236,17 +236,16 @@ function loaderVisible (isVisible) {
 // From https://stackoverflow.com/a/873856/5299903
 function createUUID () {
   // http://www.ietf.org/rfc/rfc4122.txt
-  var s = []
-  var hexDigits = '0123456789abcdef'
-  for (var i = 0; i < 36; i++) {
+  const s = [];
+  const hexDigits = '0123456789abcdef';
+  for (let i = 0; i < 36; i++) {
     s[i] = hexDigits.substr(Math.floor(Math.random() * 0x10), 1)
   }
   s[14] = '4' // bits 12-15 of the time_hi_and_version field to 0010
   s[19] = hexDigits.substr((s[19] & 0x3) | 0x8, 1) // bits 6-7 of the clock_seq_hi_and_reserved to 01
   s[8] = s[13] = s[18] = s[23] = '-'
 
-  var uuid = s.join('')
-  return uuid
+  return s.join('')
 }
 
 // From https://gist.github.com/danallison/3ec9d5314788b337b682

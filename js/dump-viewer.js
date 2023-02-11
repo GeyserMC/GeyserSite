bb464dd1d8f565454d42213a634a8674e19083f8
rtm516 <ryantmilner@hotmail.co.uk>
2023-02-11 12:53:09 +0000

    Finish dump viewer intergration

--- js/dump-viewer.js
+++ js/dump-viewer.js
@@ -1,35 +1,44 @@
-// noinspection JSUnresolvedVariable
+let id = window.location.href.split('?').pop()
+if (id === '' || id === undefined) {
+  id = window.location.hash.slice(1)
+}
 
-const id = window.location.href.split('?').pop();
+const url = 'https://dump.geysermc.org/raw/' + id
 
-const url = "https://dump.geysermc.org/raw/" + id;
-const myHeaders = new Headers();
-myHeaders.append("Content-Type", "application/json");
+function capitalizeFirstLetter (string) {
+  return string.charAt(0).toUpperCase() + string.slice(1)
+}
 
-let result = null;
+fetch(url).then((response) => response.json()).then((data) => {
+  // Check for error
+  if (data.message) {
+    throw new Error(data.message)
+  }
 
-$.get(url, function(res) {
-    res = JSON.parse(res);
-    result = res;
-    console.log(res);
+  // Versions
+  document.querySelector('#geyserVersion').textContent = data.versionInfo.version
+  document.querySelector('#javaVersion').textContent = data.versionInfo.javaVersion + ' (' + data.versionInfo.architecture + ')'
+  document.querySelector('#osVersion').textContent = data.versionInfo.operatingSystem + ' ' + data.versionInfo.operatingSystemVersion
 
-    // Versions
-    $("#geyserVersion").text(res.versionInfo.version);
-    $("#javaVersion").text(res.versionInfo.javaVersion + " (" + res.versionInfo.architecture + ")");
-    $("#osVersion").text(res.versionInfo.operatingSystem + " " + res.versionInfo.operatingSystemVersion);
+  // Geyser Versions
+  document.querySelector('#buildNumber').textContent = data.gitInfo.buildNumber
+  document.querySelector('#buildNumber').href = 'https://ci.opencollab.dev/job/GeyserMC/job/Geyser/job/master/' + data.gitInfo.buildNumber + '/'
+  document.querySelector('#commit').textContent = data.gitInfo['git.commit.id.abbrev']
+  document.querySelector('#commit').href = 'https://github.com/GeyserMC/Geyser/commit/' + data.gitInfo['git.commit.id']
+  document.querySelector('#branch').textContent = data.gitInfo['git.branch']
 
-    // Geyser Versions
-    $("#buildNumber").text(res.gitInfo['git.build.number']);
-    $("#commit").text(res.gitInfo['git.commit.id.abbrev']);
-    $("#branch").text(res.gitInfo['git.branch']);
+  // Config
+  document.querySelector('#config').textContent = JSON.stringify(data.config, null, 2)
 
-    // Config
-    $("#config").text(JSON.stringify(res.config));
+  // Platform Info
+  document.querySelector('#platformIdentifier').textContent = data.bootstrapInfo.platform
+  document.querySelector('#platformName').textContent = data.bootstrapInfo.platformName ?? capitalizeFirstLetter(data.bootstrapInfo.platform.toLowerCase())
+  document.querySelector('#platformVersion').textContent = data.bootstrapInfo.platformVersion ?? 'N/A'
+  document.querySelector('#platformAPIVersion').textContent = data.bootstrapInfo.platformAPIVersion ?? 'N/A'
+  document.querySelector('#onlineMode').textContent = data.bootstrapInfo.onlineMode ?? 'N/A'
 
-    // Platform Info
-    $("#platformIdentifier").text(res.bootstrapInfo.platform);
-    $("#platformName").text(res.bootstrapInfo.platformName);
-    $("#platformVersion").text(res.bootstrapInfo.platformVersion);
-    $("#platformAPIVersion").text(res.bootstrapInfo.platformAPIVersion);
-    $("#onlineMode").text(res.bootstrapInfo.onlineMode);
-});
+  document.querySelector('#dumpBreakdown').classList.remove('d-none')
+  document.querySelector('#statusMessage').classList.add('d-none')
+}).catch((error) => {
+  document.querySelector('#statusMessage').innerHTML = 'An error occurred while loading the dump. Please try again later.<br><br>' + error
+})

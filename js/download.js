16b0b85972e08368e8d4e13868d4da6d172fe336
rtm516 <ryantmilner@hotmail.co.uk>
2023-03-24 22:41:24 +0000

    Add build number and date to download buttons

---
+++ js/download.js
@@ -0,0 +1,25 @@
+/* global location bootstrap fetch */
+
+window.addEventListener('load', () => {
+  const hash = location.hash.replace(/^#/, '') // Remove the # from the hash
+  if (hash) {
+    const tab = document.querySelector('.nav-tabs button[data-bs-target="#' + hash + '"]')
+    if (tab) {
+      bootstrap.Tab.getOrCreateInstance(tab).show()
+    }
+  }
+
+  // Change hash for page-reload
+  document.querySelectorAll('.nav-tabs button').forEach((element) => element.addEventListener('shown.bs.tab', (e) => {
+    window.location.hash = e.target.dataset.bsTarget
+  }))
+})
+
+;['geyser', 'floodgate'].forEach(project => {
+  fetch(`https://download.geysermc.org/v2/projects/${project}/versions/latest/builds/latest`).then((response) => response.json()).then((data) => {
+    const releaseDate = new Date(data.time)
+    document.querySelectorAll(`.btn-${project}-download`).forEach((element) => {
+      element.innerHTML += `<br><span style="font-size: .8rem;">Build #${data.build} &bull; ${releaseDate.toLocaleDateString()}</span>`
+    })
+  })
+})

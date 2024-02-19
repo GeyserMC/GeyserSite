3f7e9d4fa2050d746d30cf3cf412be322686878f
rtm516 <rtm516@users.noreply.github.com>
2024-02-19 19:06:05 +0000

    Add other projects support to downloads page (#45)
    * Add other projects support to downloads page
    
    * Add error message for missing downloads
    
    * Swap unnecessary let
    
    * Support setup url for downloads
    
    * Revamp downloads to completely new format
    
    * Add title overrides for some platforms
    
    * Fix commit log on version change
    
    * Add NeoForge platform icon
    
    * Add ViaProxy icon
    
    * Add ViaProxy platform data
    
    * Update ViaProxy icon
    
    * Tidy up config
    
    * Add hash links for other downloads
    
    * Fix error when no sub project specified in hash
    
    * Fix tabs and download buttons
    
    * Add external link for downloads
    
    * Monospace commit IDs
    
    * Move to setup url for other projects
    
    * Other modal re-arrange
    
    * Update hash on modal close

--- js/download.js
+++ js/download.js
@@ -1,10 +1,23 @@
 /* global location bootstrap fetch */
 
 window.addEventListener('load', () => {
-  const hash = location.hash.replace(/^#/, '').toLowerCase() // Remove the # from the hash
-  if (hash) {
-    const tab = document.querySelector('.nav-tabs button[data-bs-target="#' + hash + '"]')
+  const hash = location.hash.replace(/^#/, '').toLowerCase().split('-') // Remove the # from the hash
+  const tabId = hash[0]
+  if (tabId) {
+    const tab = document.querySelector('.nav-tabs button[data-bs-target="#' + tabId + '"]')
     if (tab) {
+      if (tabId === 'other') {
+        const otherProject = hash[1]
+        if (otherProject) {
+          const autoSelect = () => {
+            const button = document.querySelector('button[data-bs-download="' + otherProject + '"]')
+            button.click()
+            tab.removeEventListener('shown.bs.tab', autoSelect)
+          }
+          tab.addEventListener('shown.bs.tab', autoSelect)
+        }
+      }
+
       bootstrap.Tab.getOrCreateInstance(tab).show()
     }
   }
@@ -12,14 +25,180 @@ window.addEventListener('load', () => {
   // Change hash for page-reload
   document.querySelectorAll('.nav-tabs button').forEach((element) => element.addEventListener('shown.bs.tab', (e) => {
     window.location.hash = e.target.dataset.bsTarget
+    loadBuilds(e.target.dataset.bsTarget)
   }))
+
+  const currentTabTarget = document.querySelectorAll('.nav-tabs button.active')[0].dataset.bsTarget
+  if (currentTabTarget !== '#other') {
+    loadBuilds(currentTabTarget)
+  }
 })
 
-;['geyser', 'floodgate'].forEach(project => {
-  fetch(`https://download.geysermc.org/v2/projects/${project}/versions/latest/builds/latest`).then((response) => response.json()).then((data) => {
-    const releaseDate = new Date(data.time)
-    document.querySelectorAll(`.btn-${project}-download`).forEach((element) => {
-      element.innerHTML += `<br><span style="font-size: .8rem;">Build #${data.build} &bull; ${releaseDate.toLocaleDateString()}</span>`
+// Work out the previous version from the current version
+function getPreviousVersion (version) {
+  const parts = version.split('.')
+  if (parts.length === 1) {
+    return (parseInt(parts[0]) - 1).toString()
+  }
+
+  parts[parts.length - 1] = (parseInt(parts[parts.length - 1]) - 1).toString()
+  return parts.join('.')
+}
+
+function loadBuilds (target) {
+  const tab = document.querySelector(target)
+  const project = target.substring(1)
+
+  const statusSection = tab.querySelector('#' + project + 'StatusSection')
+  const placeholder = statusSection.querySelector('.placeholder-glow')
+
+  const buildsSection = tab.querySelector('#' + project + 'BuildsSection')
+  const buildInfo = buildsSection.querySelector('.build-info')
+  const downloadButtons = buildsSection.querySelector('.download-buttons')
+  const commitLog = tab.querySelector('.commit-log')
+
+  // Recursively get the remaining commits
+  const getRemainingCommits = async (version, count, maxCommits) => {
+    const prevVer = getPreviousVersion(version)
+    const data = await fetch(`https://download.geysermc.org/v2/projects/${project}/versions/${prevVer}/builds`).then((response) => response.json())
+
+    // Reverse the builds so the latest build is first
+    data.builds.reverse()
+
+    let htmlStr = ''
+
+    // Build the changelog
+    for (const build of data.builds) {
+      if (count >= maxCommits) {
+        break
+      }
+      for (const change of build.changes) {
+        if (count >= maxCommits) {
+          break
+        }
+
+        htmlStr += `<p><a href="https://github.com/GeyserMC/${project}/commit/${change.commit}">${change.commit.substring(0, 7)}</a> &bull; ${change.summary}</p>`
+        count++
+      }
+    }
+
+    if (count < maxCommits) {
+      htmlStr += await getRemainingCommits(data.version, count, maxCommits)
+    }
+
+    return htmlStr
+  }
+
+  const processDownloadData = async (data) => {
+    if (data.error) {
+      throw new Error(data.error)
+    }
+
+    // Reverse the builds so the latest build is first
+    data.builds.reverse()
+
+    // Build the changelog
+    const maxCommits = 10
+    let count = 0
+    for (const build of data.builds) {
+      if (count >= maxCommits) {
+        break
+      }
+      for (const change of build.changes) {
+        if (count >= maxCommits) {
+          break
+        }
+
+        commitLog.innerHTML += `<p><a class="font-monospace" href="https://github.com/GeyserMC/${project}/commit/${change.commit}">${change.commit.substring(0, 7)}</a> &bull; ${change.summary}</p>`
+        count++
+      }
+    }
+
+    // If we dont have enough commits, get the previous version (recursive)
+    if (count < maxCommits) {
+      commitLog.innerHTML += await getRemainingCommits(data.version, count, maxCommits)
+    }
+
+    // Grab the latest build
+    const latestBuild = data.builds[0]
+
+    const releaseDate = new Date(latestBuild.time)
+
+    // Add the build number and release date if there is more than one download
+    buildInfo.innerHTML = `<span>Build #${latestBuild.build} &bull; ${releaseDate.toLocaleDateString()}</span>`
+
+    for (const platformId in latestBuild.downloads) {
+      let title = platformId.charAt(0).toUpperCase() + platformId.slice(1)
+      let icon = ''
+      for (const platformInfo of window.platforms) {
+        if (title.toLowerCase().includes(platformInfo.name)) {
+          if (platformInfo.title) {
+            title = platformInfo.title
+          }
+
+          icon = `<img src="${platformInfo.icon}" alt="${platformInfo.name}" class="me-1 icon">`
+          break
+        }
+      }
+
+      downloadButtons.innerHTML += `
+          <div class="col-sm-12 col-lg-6 py-2">
+            <div class="card h-100">
+              <div class="card-body">
+                  <h5 class="card-title d-flex align-items-center">${icon}${title}</h5>
+              </div>
+              <div class="card-footer d-grid">
+                <a class="btn btn-success" href="https://download.geysermc.org/v2/projects/${data.project_id}/versions/latest/builds/latest/downloads/${platformId}">
+                  <i class="bi bi-download pe-1"></i> Download
+                </a>
+              </div>
+            </div>
+          </div>`
+    }
+
+    // Add any external build links
+    if ('external_builds' in window.downloads[data.project_id]) {
+      for (const externalBuild of window.downloads[data.project_id].external_builds) {
+        let title = externalBuild.title
+        let icon = ''
+        for (const platformInfo of window.platforms) {
+          if (title.toLowerCase().includes(platformInfo.name)) {
+            if (platformInfo.title) {
+              title = platformInfo.title
+            }
+
+            icon = `<img src="${platformInfo.icon}" alt="${platformInfo.name}" class="me-1 icon">`
+            break
+          }
+        }
+
+        downloadButtons.innerHTML += `
+            <div class="col-sm-12 col-lg-6 py-2">
+              <div class="card h-100">
+                <div class="card-body">
+                    <h5 class="card-title d-flex align-items-center">${icon}${title}</h5>
+                </div>
+                <div class="card-footer d-grid">
+                  <a class="btn btn-success" href="${externalBuild.link}">
+                    <i class="bi bi-download pe-1"></i> Download
+                  </a>
+                </div>
+              </div>
+            </div>`
+      }
+    }
+
+    // Hide the placeholder
+    placeholder.style.display = 'none'
+  }
+
+  if (placeholder.style.display !== 'none') {
+    fetch(`https://download.geysermc.org/v2/projects/${project}/versions/latest/builds`).then((response) => response.json()).then((data) => {
+      processDownloadData(data)
+    }).catch((e) => {
+      console.error('Failed to fetch builds for ' + project, e)
+      placeholder.style.display = 'none'
+      statusSection.innerHTML += '<div class="alert alert-danger mb-0" role="alert">Failed to load builds</div>'
     })
-  })
-})
+  }
+}
---
+++ js/other-downloads.js
@@ -0,0 +1,100 @@
+/* global fetch */
+
+window.otherDownloadsCache = {}
+
+const otherModal = document.getElementById('otherModal')
+if (otherModal) {
+  otherModal.addEventListener('show.bs.modal', (event) => {
+    const button = event.relatedTarget
+
+    const downloadKey = button.getAttribute('data-bs-download')
+    const download = window.otherDownloads[downloadKey]
+
+    window.location.hash = 'other-' + downloadKey
+
+    const title = otherModal.querySelector('#otherModalLabel')
+    title.textContent = download.title
+
+    const info = otherModal.querySelector('#otherModalInfo')
+    info.innerHTML = download.info
+
+    const setup = otherModal.querySelector('#otherModalSetup')
+    const footer = otherModal.querySelector('#otherModalFooter')
+    if (download.setup_url) {
+      otherModal.querySelector('#otherModalSetupTitle').style.display = 'none'
+      footer.innerHTML = `
+        <a class="btn btn-block btn-info" href="${download.setup_url}">
+          <i class="bi bi-book pe-1"></i> Setup instructions
+        </a>`
+      setup.innerHTML = ''
+    } else {
+      otherModal.querySelector('#otherModalSetupTitle').style.display = 'block'
+      footer.innerHTML = ''
+      setup.innerHTML = download.setup
+    }
+
+    const placeholder = otherModal.querySelector('.placeholder-glow')
+    placeholder.style.display = 'block'
+
+    const downloadButtons = otherModal.querySelector('#otherModalDownloadBtns')
+    downloadButtons.innerHTML = ''
+
+    const downloadsTitle = otherModal.querySelector('#otherModelDownloadsTitle')
+
+    const processDownloadData = (data) => {
+      const releaseDate = new Date(data.time)
+      const downloadsCount = Object.keys(data.downloads).length
+
+      // Add the build number and release date if there is more than one download
+      if (downloadsCount > 1) {
+        downloadButtons.innerHTML += `<span>Build #${data.build} &bull; ${releaseDate.toLocaleDateString()}</span>`
+      }
+
+      for (const platformId in data.downloads) {
+        let title = data.project_name
+
+        // If there are multiple downloads, use the name of the download
+        if (downloadsCount > 1) {
+          downloadsTitle.style.display = 'block'
+          title = data.downloads[platformId].name
+        } else {
+          downloadsTitle.style.display = 'none'
+        }
+
+        let buttonHtml = ''
+
+        buttonHtml += `
+          <a class="btn btn-block btn-success" href="https://download.geysermc.org/v2/projects/${data.project_id}/versions/latest/builds/latest/downloads/${platformId}">
+            <i class="bi bi-download pe-1"></i> Download ${title}`
+
+        // Add the build number and release date if there is only one download
+        if (downloadsCount === 1) {
+          buttonHtml += `<br><span class="fs-small">Build #${data.build} &bull; ${releaseDate.toLocaleDateString()}</span>`
+        }
+
+        buttonHtml += '</a>'
+
+        downloadButtons.innerHTML += buttonHtml
+      }
+
+      // Hide the placeholder
+      placeholder.style.display = 'none'
+    }
+
+    if (window.otherDownloadsCache[downloadKey]) {
+      processDownloadData(window.otherDownloadsCache[downloadKey])
+    } else {
+      fetch(`https://download.geysermc.org/v2/projects/${downloadKey}/versions/latest/builds/latest`).then((response) => response.json()).then((data) => {
+        processDownloadData(data)
+        window.otherDownloadsCache[downloadKey] = data // Store to cache after to prevent caching errors
+      }).catch(() => {
+        placeholder.style.display = 'none'
+        downloadButtons.innerHTML = '<div class="alert alert-danger mb-0" role="alert">Failed to load downloads</div>'
+      })
+    }
+  })
+
+  otherModal.addEventListener('hidden.bs.modal', () => {
+    window.location.hash = 'other'
+  })
+}
